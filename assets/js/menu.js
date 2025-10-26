62543ee4e7094835c006e81b467831e171c1a7d4
Bruno Santos Silva <jkbrunowk@hotmail.com>
2025-10-26 10:33:24 -0300

    feat: Funcionalidades do 'menu mobile' totalmente desenvolvidas.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,31 @@
+/* -------------------- GET ELEMENTS --------------------*/
+
 const buttonExitMenuMobile = document.querySelector(".exit_button");
+const menuMobile = document.querySelector(".menu_mobile");
+const menuMobileContainer = document.querySelector(".menu_mobile_container");
+const menuMobileModalFilter = document.querySelector(
+  ".menu_mobile_modal_filter"
+);
+const menuMobileModal = document.querySelector(".menu_mobile_modal");
+
+/* -------------------- LISTENERS GET ELEMENTS -------------------- */
 
 buttonExitMenuMobile.addEventListener("click", () => {
-  alert("Fechar menu mobile");
+  toggleMenuMobile();
 });
+
+menuMobile.addEventListener("click", () => {
+  toggleMenuMobile();
+});
+menuMobileModalFilter.addEventListener("click", () => {
+  toggleMenuMobile();
+});
+
+/* -------------------- FUNCTIONS EXPORT -------------------- */
+
+const toggleMenuMobile = () => {
+  menuMobileContainer.classList.toggle("menu_mobile_opened");
+  menuMobileModalFilter.classList.toggle("menu_mobile_opened");
+  menuMobileModal.classList.toggle("menu_mobile_opened");
+  menuMobileModal.classList.toggle("menu_mobile_opened_menu_mobile_modal");
+};
